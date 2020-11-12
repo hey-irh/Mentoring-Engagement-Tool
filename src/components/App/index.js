7f048c21fb41366b66e52a7b3c20a000d5fec516
crayola-eater <a-sheikh@outlook.com>
2020-11-12 21:44:45 +0000

    Tidied nav bar CSS

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,12 +18,10 @@ function App() {
         <nav className="nav">
           <Link to="/">
             <img
-              width="100px"
-              height="auto"
-              src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+              className="createSessionLink"
+              src="/static/images/SoC_logo.png"
             ></img>
           </Link>
-
           <Link to="/faqs">
             <button className="createSessionLink">FAQs</button>
           </Link>
